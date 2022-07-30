554c22efea5c4073f217c6da57bc307d8cd8260d
Catherine E Powell <35599212+ceapowell@users.noreply.github.com>
2022-07-30 12:36:14 +0100

    Update bore_diff.m

--- diffusion3D/test_problems/bore_diff.m
+++ diffusion3D/test_problems/bore_diff.m
@@ -2,15 +2,15 @@
 % IFISS scriptfile: DJS; 29 July 2022.
 % Copyright (c) 2022 G. Papanikos, C.E. Powell, D.J. Silvester
 
-%------ define geometry
+% define geometry
 pde=1; domain=3;
-fprintf('\n\nGrid generation for bore hole domain\n')
+fprintf('\n\nGrid generation for borehole domain\n')
 nc=default('grid parameter? enter 2 for 30x30x30 grid (default 1)',1);
 bore_domain(1,nc)
 load bore_grid.mat
 grid_type = 2;
 
-%------ set up matrices
+% set up matrices
 qmethod=default('Q1/Q2 approximation 1/2? (default Q1)',1);
 % reference grid switch
 if grid_type==1  % uniform grid
@@ -24,16 +24,16 @@ else
     [A,M,f] = femq1_diff3D(xyz,ev);
 end
 
-%------ boundary conditions
+% boundary conditions
 [Agal,fgal] = nonzerobc3D(A,f,xyz,bound3D);
 
-%------ save resulting system
+% save resulting system
 fprintf('system saved in borehole_diff.mat ...\n')
 gohome
 cd datafiles
 save -v7.3 borehole_diff.mat qmethod Agal M fgal xyz x y z
 
-lin_system_choice = default('Choose between a direct or an iterative solver 1/0 (direct/iterative) (default 1)',1);
+lin_system_choice = default('Choose between direct or iterative solver 1/0 (direct/iterative) (default 1)',1);
 tic
 if lin_system_choice
     fprintf('solving linear system using direct solver... \n')
@@ -44,7 +44,7 @@ else
 end
 etoc=toc; fprintf('Galerkin system solved in %8.3e seconds\n\n',etoc)
 
-%------ compute a posteriori error estimate and plot solution
+% compute a posteriori error estimate and plot solution
 if qmethod==1,
     diffpost3D
     errplot3D(x_it,error_tot,ev,xyz,x,y,z,99),

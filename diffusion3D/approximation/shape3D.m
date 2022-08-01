46377a9556d9b2ca81a9b6f4e0c3a6f26266bb2d
Catherine E Powell <35599212+ceapowell@users.noreply.github.com>
2022-08-01 13:12:34 +0100

    Update shape3D.m

--- diffusion3D/approximation/shape3D.m
+++ diffusion3D/approximation/shape3D.m
@@ -1,15 +1,16 @@
  function [phi,dphids,dphidt,dphidl] = shape3D(s,t,l)
-%SHAPE evaluates trilinear shape functions 
-%   [phi,dphids,dphidt,,dphidl] = shape3D(s,t,l);
-%   input
+%SHAPE3D evaluates trilinear shape functions 
+%   [phi,dphids,dphidt,dphidl] = shape3D(s,t,l);
+%   inputs:
 %          s         x coordinate   
 %          t         y coordinate
 %          l         z coordinate 
-%   output
+%   outputs:
 %          phi        shape function
 %          dphids     x derivative of phi
 %          dphidt     y derivative of phi
 %          dphidl     z derivative of phi 
+%
 %                                         l
 %           8 _ _ _ _7                    |                      
 %          /|       /|                    |
@@ -19,10 +20,11 @@
 %        | /      | /                  /t
 %        |/_ _ _ _|/  
 %        1       2
+%
 % IFISS function: GP; 9 June 2022.
 % Copyright (c)  2022  G.Papanikos,  C.E. Powell, D.J. Silvester
       one = 1.0e0;
-%     trilinear bases functions 
+%     trilinear basis functions 
       phi(1,:) = 1/8 *(one-s).*(one-t).*(one-l);  %(-1,-1,-1) 1
       phi(2,:) = 1/8 *(one+s).*(one-t).*(one-l);  %( 1,-1,-1) 2
       phi(3,:) = 1/8 *(one+s).*(one+t).*(one-l);  %( 1, 1,-1) 3
@@ -41,7 +43,6 @@
       dphids(6,:) =  1/8 *(one-t).*(one+l);
       dphids(7,:) =  1/8 *(one+t).*(one+l);
       dphids(8,:) = -1/8 *(one+t).*(one+l);
-      
       
       % derivatives with respect to t
       dphidt(1,:) = -1/8 *(one-s).*(one-l);
@@ -62,6 +63,5 @@
       dphidl(6,:) =  1/8 *(one+s).*(one-t);
       dphidl(7,:) =  1/8 *(one+s).*(one+t);
       dphidl(8,:) =  1/8 *(one-s).*(one+t);
-      
       
       return

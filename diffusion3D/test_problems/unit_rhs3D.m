651a7b3cbb6d73454ef32818b6c086d976a3e948
Catherine E Powell <35599212+ceapowell@users.noreply.github.com>
2022-08-01 10:39:54 +0100

    Update unit_rhs3D.m

--- diffusion3D/test_problems/unit_rhs3D.m
+++ diffusion3D/test_problems/unit_rhs3D.m
@@ -1,11 +1,12 @@
 function f = specific_rhs3D(x,y,z,nel,ngpt)
-%unit_rhs   unit RHS forcing function
+%UNIT_RHS3D evaluates source function f=1
 %   f = specific_rhs(x,y,z,nel)
-%   input
+%   inputs:
 %          x          x coordinate vector
 %          y          y coordinate vector 
 %          z          y coordinate vector 
 %          nel        number of elements  
+%          ngpt       ???
 % IFISS function: GP; 9 June 2022.
 % Copyright (c)  2022  G.Papanikos,  C.E. Powell, D.J. Silvester
 

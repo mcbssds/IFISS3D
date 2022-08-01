87b141df98d4422acceabcfae383cc95d20d5bba
Catherine E Powell <35599212+ceapowell@users.noreply.github.com>
2022-08-01 10:05:27 +0100

    Update stair_diff.m

--- diffusion3D/test_problems/stair_diff.m
+++ diffusion3D/test_problems/stair_diff.m
@@ -10,7 +10,7 @@ load stair_grid.mat
 % set up matrices
 qmethod=default('Q1/Q2 approximation 1/2? (default Q1)',1);
 % reference grid switch
-savesol=default('save results for reference 1/0 (yes/no)? (default no)',0);
+savesol=default('Save results for reference 1/0 (yes/no)? (default no)',0);
 if qmethod ==2,
     [x,y,z,xyz] = q2grid3D(x,y,z,xyz,mv,bound3D);
     [A,M,f] = femq2_diff3D(xyz,mv);
@@ -23,7 +23,7 @@ end
 [Agal,fgal] = nonzerobc3D(A,f,xyz,bound3D);
 
 % save resulting system
-fprintf('system saved in stair_diff.mat ...\n')
+fprintf('System saved in stair_diff.mat ...\n')
 gohome
 cd datafiles
 save -v7.3 stair_diff.mat qmethod Agal M fgal xyz x y z

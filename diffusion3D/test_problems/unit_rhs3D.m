627bf15805177a5885c1af19aa2d5626c3f56de4
Catherine E Powell <35599212+ceapowell@users.noreply.github.com>
2022-08-01 10:41:24 +0100

    Update unit_rhs3D.m

--- diffusion3D/test_problems/unit_rhs3D.m
+++ diffusion3D/test_problems/unit_rhs3D.m
@@ -7,7 +7,7 @@ function f = specific_rhs3D(x,y,z,nel,ngpt)
 %          z          y coordinate vector 
 %          nel        number of elements  
 %          ngpt       ???
-% IFISS function: GP; 9 June 2022.
+% IFISS function: CEP; 1st August 2022.
 % Copyright (c)  2022  G.Papanikos,  C.E. Powell, D.J. Silvester
 
  f=ones(nel,ngpt);

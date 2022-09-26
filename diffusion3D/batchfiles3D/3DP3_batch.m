2483579cbcf509d592c150890dac742ff0a08696
David J. Silvester <d.silvester@manchester.ac.uk>
2022-09-26 18:36:19 +0100

    preparation for pubic release

--- diffusion3D/batchfiles3D/3DP3_batch.m
+++
@@ -1,7 +0,0 @@
-3     % problem
-2     % grid parameter
-0     % direct solver switch 1/0
-2     % error estimation strategy
-0     % include boundary correction 1/0
-
-%% data file for third test problem
